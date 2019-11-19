1b17e53e01bea2f4ae639dd558dad17104df51b5
Navid Shaikh <nshaikh@redhat.com>
2019-11-19 16:49:07 +0530

    Update to knative released version setup script
     - Uncomment eventing setup
     - Scale few istio replicas down to 1
     - Update the kube version to 1.14

--- knative-setup/minikube/knative-setup.sh
+++ knative-setup/minikube/knative-setup.sh
@@ -15,10 +15,11 @@ else
   reset=''
 fi
 
-serving_version="v0.9.0"
-eventing_version="v0.9.0"
+serving_version="v0.10.0"
+eventing_version="v0.10.1"
 eventing_sources_version="v0.9.0"
-istio_version="1.1.13"
+istio_version="1.2.7"
+#kube_version="v1.13.4"
 kube_version="v1.14.0"
 
 MEMORY="$(minikube config view | awk '/memory/ { print $3 }')"
@@ -33,11 +34,11 @@ function header_text {
 header_text "Starting Knative on minikube!"
 header_text "Using Kubernetes Version:               ${kube_version}"
 header_text "Using Knative Serving Version:          ${serving_version}"
-#header_text "Using Knative Eventing Version:         ${eventing_version}"
-header_text "Using Knative Eventing Sources Version: ${eventing_sources_version}"
+header_text "Using Knative Eventing Version:         ${eventing_version}"
+#header_text "Using Knative Eventing Sources Version: ${eventing_sources_version}"
 header_text "Using Istio Version:                    ${istio_version}"
 
-minikube start --memory="${MEMORY:-16638}" --cpus="${CPUS:-6}" --kubernetes-version="${kube_version}" --vm-driver="${DRIVER:-kvm2}" --disk-size="${DISKSIZE:-50g}" --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
+minikube start --memory="${MEMORY:-12288}" --cpus="${CPUS:-4}" --kubernetes-version="${kube_version}" --vm-driver="${DRIVER:-kvm2}" --disk-size="${DISKSIZE:-30g}" --extra-config=apiserver.enable-admission-plugins="LimitRanger,NamespaceExists,NamespaceLifecycle,ResourceQuota,ServiceAccount,DefaultStorageClass,MutatingAdmissionWebhook"
 header_text "Waiting for core k8s services to initialize"
 sleep 5; while echo && kubectl get pods -n kube-system | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
 
@@ -46,13 +47,18 @@ err=1
 while [ $err -eq 1 ]; do
   curl -L "https://raw.githubusercontent.com/knative/serving/${serving_version}/third_party/istio-${istio_version}/istio-lean.yaml" \
       | sed 's/LoadBalancer/NodePort/' \
-      | kubectl apply --validate=false --overwrite=true --filename -
+      | kubectl apply --overwrite=true --filename -
   err=$?
 done
 
+# scale deployment/cluster-local-gateway to 1 replica
+kubectl scale deployment/cluster-local-gateway --replicas=1 -n istio-system
+# scale deployment/istio-ingressgateway to 1 replica
+kubectl scale deployment/istio-ingressgateway --replicas=1 -n istio-system
+
 # Label the default namespace with istio-injection=enabled.
 header_text "Labeling default namespace w/ istio-injection=enabled"
-kubectl label namespace default istio-injection=enabled --overwrite=true
+kubectl label --overwrite=true namespace default istio-injection=enabled --overwrite=true
 header_text "Waiting for istio to become ready"
 sleep 5; while echo && kubectl get pods -n istio-system | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
 
@@ -65,10 +71,10 @@ header_text "Waiting for Knative Serving to become ready"
 sleep 5; while echo && kubectl get pods -n knative-serving | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
 
 
-#header_text "Setting up Knative Eventing"
-#kubectl apply --overwrite=true --filename https://github.com/knative/eventing/releases/download/${eventing_version}/release.yaml
+header_text "Setting up Knative Eventing"
+kubectl apply --overwrite=true --filename https://github.com/knative/eventing/releases/download/${eventing_version}/release.yaml
 #kubectl apply --filename https://raw.githubusercontent.com/openshift-knative/knative-eventing-operator/master/deploy/resources/knative-eventing-sources-${eventing_sources_version}.yaml
 
-#header_text "Waiting for Knative Eventing to become ready"
-#sleep 5; while echo && kubectl get pods -n knative-eventing | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
+header_text "Waiting for Knative Eventing to become ready"
+sleep 5; while echo && kubectl get pods -n knative-eventing | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
 #sleep 5; while echo && kubectl get pods -n knative-sources | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
